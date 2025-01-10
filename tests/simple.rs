dd38b0e918b8e6c13ce225013f4baf0cbb18b404
xamgore <xamgore@ya.ru>
2025-01-11 00:07:55 +0300

    Compare outputs

---
+++ tests/simple.rs
@@ -0,0 +1,20 @@
+use segtok::segmenter::split_multi;
+use segtok::tokenizer::{split_contractions, web_tokenizer};
+
+#[test]
+fn simple() {
+    let input = r#"I am a competition-centric person! I really like competitions. Every competition is a hoot!"#;
+
+    let sentences: Vec<Vec<_>> = split_multi(input, Default::default())
+        .into_iter()
+        .filter(|span| !span.is_empty())
+        .map(|span| {
+            split_contractions(web_tokenizer(&span))
+                .into_iter()
+                .filter(|word| !(word.is_empty() || word.len() > 1 && word.starts_with("'")))
+                .collect()
+        })
+        .collect();
+
+    println!("{:?}", sentences);
+}
